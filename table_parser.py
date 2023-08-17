21df63e30e904fc7a628e22acf488698567b0396
Anna Kelmanson <kelmannson@gmail.com>
2023-08-18 00:08:12 +0200

    Tailored

--- table_parser.py
+++ table_parser.py
@@ -1,24 +1,54 @@
 import pandas as pd
-from tkinter import filedialog
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-filenames = filedialog.askopenfilenames()
+def load_csv(file_path):
+    return pd.read_csv(file_path)
 
-dfs = []
+def add_window_flag_rows(df):
+    window_values = df.columns[1:]
+    window_row = ['window'] + [col.split('_')[-2] for col in window_values]
+    flag_row = ['flag'] + [col.split('_')[-1] for col in window_values]
+    
+    window_df = pd.DataFrame([window_row], columns=df.columns)
+    flag_df = pd.DataFrame([flag_row], columns=df.columns)
+    
+    return pd.concat([window_df, df, flag_df], ignore_index=True)
 
-for f in filenames:
+def transpose_and_process(df):
+    df_T = df.transpose()
+    df_T.columns = df_T.iloc[0]
+    df_T = df_T[1:]
+    
+    df_T['type'] = 'test'
+    
+    df_T['CC*'] = df_T['CC*'].str.replace(r'\s*\([^)]*\)', '', regex=True)
+    
+    return df_T
 
-  df = pd.read_csv(f, sep=';', index_col=0)
+def filter_by_window(df, window):
+    return df[df['window'] == window]
 
-  parts = df.columns[0].split('_')
-  window = parts[4]
-  df.loc['window'] = [window] * len(df.columns)
+def plot_scatter(df):
+    unique_flags = df['flag'].unique()
 
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(10, 6))
+    plot = sns.scatterplot(x='flag', y='CC*', hue='type', data=df)
 
-  dfs.append(df)
+    plot.set_title(f"Window {df['window'].unique()[0]}")
+    plt.xticks(unique_flags)
+    plt.legend(title='type')
+    
+    plt.show()
+    
+def process_csv(file_path):
+    df = load_csv(file_path)
+    df = add_window_flag_rows(df)
+    df_T = transpose_and_process(df)
+    
+    for window in df_T['window'].unique():
+        df_window = filter_by_window(df_T, window)
+        plot_scatter(df_window)
 
-
-for df in dfs:
-  
-  print(df.to_string()) # Конверт в строку
-
-  print('\n') # Пустая строка между DataFrame
+process_csv('2_flag.csv')
